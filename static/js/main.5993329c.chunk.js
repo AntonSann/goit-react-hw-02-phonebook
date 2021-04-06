868eb150aa9d6381d794b9ed60354f1dab6c6b34
Anton Sannykov <antonsannykov@gmail.com>
2021-04-06 23:40:04 +0300

    Updates

---
+++ static/js/main.5993329c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var o=n(1),r=n.n(o),c=n(3),s=n.n(c),a=n(0);var i=function(){return Object(a.jsx)(a.Fragment,{})};s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.5993329c.chunk.js.map
